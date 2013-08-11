31cf053eb9049e562a2600638c12b6d1cb5c2950
The Doctor <drwho@virtadpt.net>
2013-08-11 14:51:16 -0400

    Added argument vector parser.
    Signed-off-by: The Doctor <drwho@virtadpt.net>

--- couchdb/sql_to_nosql.pl
+++ couchdb/sql_to_nosql.pl
@@ -7,11 +7,61 @@
 # This script takes as its arguments one or more SQL files and outputs one
 # JSON dump which includes the converted contents of all of the SQL files.
 
+# Modules.
+use Getopt::Long;
+
 # Variables
 my $database = 'ushahidi';
+my $help, $username, $host, $password;
+
 my @tables = ('actions', 'actions_log', 'alert', 'alert_category', 'alert_sent', 'api_banned', 'api_log', 'api_settings', 'badge', 'badge_users', 'category', 'category_lang', 'checkin', 'city', 'cluster', 'comment', 'country', 'externalapp', 'feed', 'feed_item', 'form', 'form_field', 'form_field_option', 'form_response', 'geometry', 'incident', 'incident_category', 'incident_lang', 'incident_person', 'layer', 'level', 'location', 'maintenance', 'media', 'message', 'openid', 'page', 'permissions', 'permissions_roles', 'plugin', 'private_message', 'rating', 'reporter', 'roles', 'roles_users', 'scheduler', 'scheduler_log', 'service', 'sessions', 'settings', 'user_devices', 'user_tokens', 'users', 'verified');
+
+# This script needs some command line args, so set up an argument vector parser.
+GetOptions(	"help"		=> \$help,
+		"username=s"	=> \$username,
+		"host=s"	=> \$host,
+		"password=s"	=> \$password);
+
+# Parse the arguments passed to the script.
+# User has explicitly asked for help.
+if ($help) {
+    help();
+    exit (0);
+    }
+
+# No username.
+if (! $username) {
+    print "ERROR: No MySQL database username to connect with.\n";
+    help();
+    exit (1);
+    }
+chomp $username;
+
+# No hostname.
+if (! $host) {
+    $host = 'localhost';
+    }
+chomp $host;
+
+# No MySQL password.
+if (! $password) {
+    print "ERROR: No MySQL database password to connect with.\n";
+    help();
+    exit (1);
+    }
+chomp $password;
+
+
 
 foreach $table (@tables) {
     print "Table: " . $table . "\n";
+    }
+
+# End of script.
+exit (0);
+
+# Function that displays help text to the user.  Catch-all.
+sub help {
+    print "USAGE: $0 --username=username (--host=host) --password=password";
     }
 
